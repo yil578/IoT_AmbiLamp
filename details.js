e6b867f1fc57b83d1d83f5066f69ca30a1362d87
yil578 <yil578@ucsd.edu>
2018-05-18 16:03:45 -0700

    index and details web pages

---
+++ details.js
@@ -0,0 +1,103 @@
+var tempCtx;
+var tempChart;
+var tempCounter = 0;
+
+function drawTemp() {
+    tempCounter++;
+    tempCtx = document.getElementById("temp-chart-long").getContext('2d');
+     
+    if ((tempCounter = tempCounter % 2) == 0) {
+        tempChart.destroy();
+    } else {
+        tempChart = new Chart(tempCtx, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                },
+                responsive:false
+            }
+        });
+    }
+    
+}
+
+var soundCtx;
+var soundChart;
+var soundCounter = 0;
+
+function drawSound() {
+    soundCounter++;
+    soundCtx = document.getElementById("sound-chart-long").getContext('2d');
+
+    if ((soundCounter = soundCounter % 2) == 0) {
+        soundChart.destroy();
+    } else {
+        soundChart = new Chart(soundCtx, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                },
+                responsive:false
+            }
+        });  
+    }
+}
+    
